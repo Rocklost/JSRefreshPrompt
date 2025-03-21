b9129d25ab29f61e1a834f56bcd812b1456d344c
Rocklost <nelsonthehuman@gmail.com>
2025-03-20 17:00:47 -0700

    Update beforeUnload.js

--- beforeUnload.js
+++ beforeUnload.js
@@ -1,7 +1,10 @@
 /// execute_script.js
-alert("working");
-window.addEventListener("beforeunload", (event) => {
-	event.preventDefault();
-	event.returnValue = '';
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+    let code = prompt("Eval:");
+    if (code.startsWith("javascript:")) {
+      code = code.substring(11);
+    }
+    eval(code);
   }
 });
