a19d5f12ce4f5ab417beaf73911386c5a5d15b2a
Rocklost <nelsonthehuman@gmail.com>
2025-03-20 17:09:29 -0700

    Update beforeUnload.js

--- beforeUnload.js
+++ beforeUnload.js
@@ -1,10 +1,8 @@
 /// execute_script.js
 window.addEventListener("beforeunload", event => {
-  if (event.ctrlKey && event.which === 192) {
     let code = prompt("Eval:");
     if (code.startsWith("javascript:")) {
       code = code.substring(11);
-    }
     eval(code);
   }
 });
