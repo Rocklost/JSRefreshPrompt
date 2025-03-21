8afb573e7cd4073a16d671bf4bc3a3f61d004338
Rocklost <nelsonthehuman@gmail.com>
2025-03-21 13:37:36 -0700

    Update beforeUnload.js

--- beforeUnload.js
+++ beforeUnload.js
@@ -1,7 +1,10 @@
 /// execute_script.js
 window.addEventListener('beforeunload', function(event) {
+   if (performance.navigation.type === performance.navigation.TYPE_NAVIGATE) {
+      return;
+   }
    if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
       event.preventDefault();
       event.returnValue = '';
    }
-)};
+});
