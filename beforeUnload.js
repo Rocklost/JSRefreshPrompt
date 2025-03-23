015e0e366da0b3c68fb680238b7ea5d67d1ef365
Rocklost <nelsonthehuman@gmail.com>
2025-03-23 16:05:14 -0700

    Update beforeUnload.js

--- beforeUnload.js
+++ beforeUnload.js
@@ -1,31 +1,7 @@
 /// execute_script.js
-function navigationType(){
-
-    var result;
-    var p;
-
-    if (window.performance.navigation) {
-        result=window.performance.navigation;
-        if (result==255){result=4}
-    }
-
-    if (window.performance.getEntriesByType("navigation")){
-       p=window.performance.getEntriesByType("navigation")[0].type;
-
-       if (p=='navigate'){result=0}
-       if (p=='reload'){result=1}
-       if (p=='back_forward'){result=2}
-       if (p=='prerender'){result=3}
-    }
-    return result;
-}
-
-function navigationType();
-if (result=0 || result=2 || result=3) {
-   return;
-} else {
-   if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
-      event.preventDefault();
-      event.returnValue = '';
-   }
-}
+window.addEventListener('beforeunload', function (event) {
+  if (performance.navigation.type === 1) {
+    event.preventDefault();
+    event.returnValue = '';
+  }
+});
